9f6cf5c8892106ab52dcededa8aa11c232f3f7db
Arnob <arnob_talukder@hotmail.com>
2018-10-27 00:14:14 -0400

    first

---
+++ src/root/Base.java
@@ -0,0 +1,11 @@
+package root;
+
+public class Base {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		Ext1.print();
+	}
+
+}
---
+++ src/root/Ext1.java
@@ -0,0 +1,7 @@
+package root;
+
+public class Ext1 {
+	public static void print() {
+		System.out.println("Ext1");
+	}
+}
